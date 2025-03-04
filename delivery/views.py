d919d3362eb9b0ae5af20a233177bed62b1bb2de
Priya9096 <priyakhairate0000@gmail.com>
2025-03-04 21:09:57 +0530

    added new files

--- delivery/views.py
+++ delivery/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.http import HttpResponse
-from delivery.models import Customer, Restaurants, Menu
+from delivery.models import Customer, Restaurants, Menu, Cart
 from delivery.forms import ResForm, MenuForm
+from django.conf import settings
+import razorpay
 # Create your views here.
 def index(request):
     return render(request,'delivery/index.html')
@@ -13,6 +16,7 @@ def sign_up(request):
     return render(request,'delivery/sign_up.html')
 
 def handle_signin(request):
+    li = Restaurants.objects.all()
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
@@ -21,7 +25,7 @@ def handle_signin(request):
             if username == 'admin':
                 return render(request, 'delivery/success.html')
             else:
-                return render(request, 'delivery/customer_home.html')
+                return render(request, 'delivery/cusdisplay_res.html',{'username':username,'li':li})
         except:
             return render(request, 'delivery/failed.html')
     
@@ -74,11 +78,72 @@ def delete_menu(request,id):
     item.delete()
     return redirect('delivery:view_menu',res_id)
 
-def cusdisplay_res(request):
+def cusdisplay_res(request, username):
+    customer = Customer.objects.get(username=username)
     li = Restaurants.objects.all()
-    return render(request, 'delivery/cusdisplay_res.html',{'li':li})
+    return render(request, 'delivery/cusdisplay_res.html',{'li':li, 'username':username})
 
-def cusmenu(request,id):
+def cusmenu(request,id, username):
     res = Restaurants.objects.get(pk=id)
     menu = Menu.objects.filter(res=res)  
-    return render(request, 'delivery/cusmenu.html',{'res':res, 'menu':menu})
+    return render(request, 'delivery/cusmenu.html',{'res':res, 'menu':menu, 'username':username})
+
+def show_cart(request, username):
+    customer = Customer.objects.get(username=username)
+    cart = Cart.objects.filter(customer= customer).first()
+    items = cart.items.all() if cart else []
+    total_price = cart.total_price() if cart else 0
+    return render(request, 'delivery/show_cart.html', {'items':items, 'total_price':total_price,'username':username})
+
+def add_to_cart(request,username,menuid):
+    customer = Customer.objects.get(username = username)
+    item = Menu.objects.get(pk=menuid)
+    cart, created = Cart.objects.get_or_create(customer = customer)
+    cart.items.add(item)
+    messages.success(request,f"{item.item_name} added")
+    return redirect('delivery:cusmenu', id = item.res.id,username=username)
+
+def checkout(request, username):
+    customer = Customer.objects.get(username = username)
+    cart = Cart.objects.filter(customer = customer).first()
+    cart_items = cart.items.all() if cart else []
+    total_price = cart.total_price() if cart else 0
+
+    if total_price == 0:
+        return render(request, 'delivery/checkout.html',{'error':'Your cart is Empty'})
+
+    client = razorpay.Client(auth=(settings.RAZORPAY_KEY_ID, settings.RAZORPAY_KEY_SECRET))
+
+    order_data = {
+        'amount':int(total_price * 100),
+        'currency':'INR',
+        'payment_capture':'1',
+    }
+    order = client.order.create(data = order_data)
+    return render(
+        request, 'delivery/checkout.html',
+        {'username':username,
+        'cart_items':cart_items,
+        'total_price':total_price,
+        'razorpay_key_id':settings.RAZORPAY_KEY_ID,
+        'order_id':order['id'],
+        'amount':total_price,
+        }
+    )
+
+def orders(request, username):
+        customer = Customer.objects.get(username = username)
+        cart = Cart.objects.filter(customer=customer).first()
+        cart_items = cart.items.all() if cart else []
+        total_price = cart.total_price() if cart else 0
+
+        if cart:
+            cart.items.clear()
+
+        return render(request, 'delivery/orders.html',{
+        'username':username,
+        'cart_items':cart_items,
+        'total_price':total_price,
+        'customer':customer
+        })
+
